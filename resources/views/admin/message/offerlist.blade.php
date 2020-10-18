62325251fd2f2e682eaa872faac9a03bffa85db3
daiki1008 <kwd.jhm@gmail.com>
2020-10-18 16:43:58 +0900

    更新21

--- resources/views/admin/message/offerlist.blade.php
+++ resources/views/admin/message/offerlist.blade.php
@@ -6,10 +6,6 @@
     @foreach($userinfos as $userinfo)
     @if($userinfo->check == "0")
     <div class="profile-index" style="background-color:#FFF5EE ;">
-      @elseif($userinfo->check == "1")
-      <div class="profile-index" style="background-color:#FFFFF0 ;">
-        @endif
-
         <a href="  {{ action('Admin\ProfileController@offermessage',['id'=> $userinfo->id,'message'=> $userinfo->message]) }}  ">
 
           <div class="index-image"><img src="{{ asset(  $userinfo['profile_image_path']  ) }}"></div>
@@ -27,7 +23,24 @@
          </div>
        </a>
      </div>
-    </div>
+
+    @elseif($userinfo->check == "1")
+    <div class="profile-index" style="background-color:#FFFFF0 ;">
+      <a href="  {{ action('Admin\ProfileController@offermessage',['id'=> $userinfo->id,'message'=> $userinfo->message]) }}  ">
+        <div class="index-image"><img src="{{ asset(  $userinfo['profile_image_path']  ) }}"></div>
+        <div class="profile-index-head">
+          <div class="index-name">{{ $userinfo->name }}</div>
+          <div class="index-tag">{{ $userinfo->tag }}</div>
+          <div class="index-introduction">{{ $userinfo->message }}</div>
+        </div>
+        <div class="profile-index-function">
+  <!-- <button type="button" class="favorite-btn" attr="{{ $userinfo->id }}" status="{{ $userinfo['status'] }}">
+    <p>Favorite</p>
+  </button> -->
+       </div>
+     </a>
+     </div>
+      @endif
      @endforeach
    </div>
  </div>
